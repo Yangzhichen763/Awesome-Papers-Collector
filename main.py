af08987b44571580e769f116c9a012c58fdf1606
志辰 杨 <oringec@163.com>
2024-12-04 19:08:39 +0800

    修改 main.py 中的内容为爬取生成 awesome 列表的代码

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-from core.arxiv_crawler import parse_arxiv_html
-from core.website_search import search_urls_by_title
+from core.awesome import awesome_search
 
 
 if __name__ == '__main__':
-    # 文章关键词
-    query = 'Attention is all you need'
+    keyword = 'Relighting'
 
-    # 爬取文章链接
-    urls = search_urls_by_title(query, "arXiv")
+    # 搜索关键词匹配的论文并生成 markdown 文件
+    awesome_search.search(keyword)
 
-    # 爬取文章摘要并生成 md 文档
-    for url in urls["arXiv"]:
-        overview = parse_arxiv_html(url)
-        overview.make()
+    # 筛除关键词匹配的论文并生成 markdown 文件
+    filter_keywords = ['NeRF', 'Gaussian Splatting', 'Neural Radiance Fields', 'Neural', 'Gaussian',
+                       'Geometry', 'Geometric', 'Material', 'Render', 'Radiance Field', '3D']
+    awesome_search.filter_title(filter_keywords, keyword)
